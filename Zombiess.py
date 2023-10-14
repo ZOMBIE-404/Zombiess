0aa4bc2f5d8245f368752587cb22884bd0a26388
ZOMBIE-404 <143811328+ZOMBIE-404@users.noreply.github.com>
2023-10-15 00:30:15 +0500

    Update Zombiess.py

--- Zombiess.py
+++ Zombiess.py
@@ -1,1 +1,6 @@
-
+import os,platform
+os.system('git pull')
+#exit('\n Wait Working On Tool..!')
+adina=platform.architecture()[0]
+if adina=="32bi":exit(" coming soon 32 bit ")
+elif adina=="64bit":__import__("BS")
